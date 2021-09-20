ceb540f8192c6bd2484b79d923c2136cff234bc0
margaritasing <61855440+margaritasing@users.noreply.github.com>
2021-09-20 15:26:20 -0300

    agrego post y get al controller

--- src/main/java/com/margaritasing/springwithreact/Controller/StudentController.java
+++ src/main/java/com/margaritasing/springwithreact/Controller/StudentController.java
@@ -1,10 +1,11 @@
 package com.margaritasing.springwithreact.Controller;
 
+import com.margaritasing.springwithreact.Model.Student;
 import com.margaritasing.springwithreact.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -16,6 +17,17 @@ public class StudentController {
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> list(){
+        return studentService.getAllStudents();
     }
 
 
